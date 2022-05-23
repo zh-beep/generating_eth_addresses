fd1f0ef016b8540a0f4c2d03088203221a1b1c27
dell <zanir.habib@gmail.com>
2022-05-23 11:43:46 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random 
+#import pysha3 
+import sha3
+#this is for the keccak algo
+
+
+#generate private key
+for 
+
+    #generate random number between 0 & 15
+    digit = random.randint(0,15)
+    #convert to hex 
+    hex_digit = hex(digit)
+    #convert to string
+    string_digit = bytes.fromhex(hex_digit)
+    #concatenate
+
+    #print string
