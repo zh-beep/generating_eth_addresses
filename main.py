8b693c33f72cb252f0ca8909a7bf1a72b99d5b30
Zanir Habib <zanirhabib@Zanirs-MBP.attlocal.net>
2022-06-07 02:24:14 -0500

    Have a string of hex

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import random
 import sha3
 #this is for the keccak algo
 
-
+full = ''
 #generate private key
-for 
-
+for i in range(10): 
     #generate random number between 0 & 15
     digit = random.randint(0,15)
     #convert to hex 
     hex_digit = hex(digit)
+    full = full + hex_digit
     #convert to string
-    string_digit = bytes.fromhex(hex_digit)
-    #concatenate
+print(full)
+#string_digit = bytes.fromhex(hex_digit)
+#concatenate
 
     #print string
