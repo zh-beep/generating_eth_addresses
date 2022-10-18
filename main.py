1268b002ad166ba8c1abf8bf52ae03c5cc3bfc61
Zanir Habib <zanirhabib@Zanirs-MBP.attlocal.net>
2022-10-18 03:21:04 -0500

    Cleaning up:

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import random 
+import random
+import cffi 
 #import pysha3 
 import sha3
 #this is for the keccak algo
 
-full = '0x'
+from coincurve import PublicKey
+
+full = ''
 #generate private key
 #better way is to make your own array/mapping and add it individually
 hex_array = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
@@ -13,8 +16,11 @@ for i in range(64):
     full = full + hex_array[digit]
     #convert to hex 
     #convert to string
-print(full)
-print(len(full))
+#convert string to hexadecimal
+hex_value = hex(int(full,16))
+hex_bytes = bytearray.fromhex(full)
+hex_char = ffi.new("char []", hex_value)
+public_key = PublicKey.from_valid_secret(hex_char).format(compressed=False)[1:]
 #string_digit = bytes.fromhex(hex_digit)
 #concatenate
 
